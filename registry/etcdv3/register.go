9eb0b7a73ed1fb979bf292a2a86357c416874c7e
luoyancn <luoyan_cn@outlook.com>
2018-07-13 18:43:48 +0800

    Add Service Discovery implements of etcdv3 for dubhe

--- registry/etcdv3/register.go
+++ registry/etcdv3/register.go
@@ -1,1 +1,60 @@
 package etcdv3
+
+import (
+	"strings"
+
+	"github.com/luoyancn/dubhe/logging"
+
+	etcd "github.com/coreos/etcd/clientv3"
+	"golang.org/x/net/context"
+)
+
+var deregister = make(chan struct{})
+
+type Option struct {
+	RegistryDir string
+	ServiceName string
+	NodeID      string
+	NData       string
+}
+
+func Register(config etcd.Config, opt Option) error {
+	key := strings.Join(
+		[]string{opt.RegistryDir, opt.ServiceName, opt.NodeID}, "/")
+	logging.LOG.Infof("Register service with key :%s\n", key)
+	client, err := etcd.New(config)
+	if err != nil {
+		panic(err)
+	}
+	go func() {
+		<-deregister
+		logging.LOG.Infof("Deleting %s from etcd...\n", key)
+		client.Delete(context.Background(), key)
+		deregister <- struct{}{}
+	}()
+
+	resp, err := client.Grant(context.TODO(), int64(10))
+	if err != nil {
+		logging.LOG.Errorf("Failed connect to etcd:%v\n", err)
+		return err
+	}
+
+	if _, err = client.Put(context.TODO(), key,
+		opt.NData, etcd.WithLease(resp.ID)); err != nil {
+		logging.LOG.Errorf(
+			"Failed Put the key %s value %sinto etcd:%v\n", key, opt.NData, err)
+		return err
+	}
+
+	if _, err := client.KeepAlive(context.TODO(), resp.ID); err != nil {
+		logging.LOG.Errorf(
+			"Failed refresh the key %s exsit in etcd:%v\n", key, err)
+		return err
+	}
+	return nil
+}
+
+func UnRegister() {
+	deregister <- struct{}{}
+	<-deregister
+}
--- registry/etcdv3/resolver.go
+++ registry/etcdv3/resolver.go
@@ -1,1 +1,38 @@
 package etcdv3
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/luoyancn/dubhe/logging"
+
+	etcd "github.com/coreos/etcd/clientv3"
+	"google.golang.org/grpc/naming"
+)
+
+type etcdResolver struct {
+	config      etcd.Config
+	registryDir string
+	serviceName string
+}
+
+func NewResolver(registryDir string, serviceName string,
+	cfg etcd.Config) naming.Resolver {
+	return &etcdResolver{registryDir: registryDir,
+		serviceName: serviceName, config: cfg}
+}
+
+func (this *etcdResolver) Resolve(target string) (naming.Watcher, error) {
+	if this.serviceName == "" {
+		return nil, errors.New("no service name provided")
+	}
+	client, err := etcd.New(this.config)
+	if err != nil {
+		logging.LOG.Errorf("Cannot init etcd cluster:%v\n", err)
+		return nil, err
+	}
+
+	key := fmt.Sprintf("%s/%s", this.registryDir, this.serviceName)
+	logging.LOG.Debugf("Watching the key named:%s\n", key)
+	return newEtcdWatcher(key, client), nil
+}
