dfdadac2f1fb8c4e419dc8e0d7d0b38cf1b39448
luoyancn <luoyan_cn@outlook.com>
2018-07-17 19:02:21 +0800

    Add grpc server/client librarys

--- grpclib/client.go
+++ grpclib/client.go
@@ -1,1 +1,60 @@
 package grpclib
+
+import (
+	"sync"
+
+	"google.golang.org/grpc"
+)
+
+type grpcPool struct {
+	conn chan *grpc.ClientConn
+	addr string
+}
+
+var gonce sync.Once
+var pool *grpcPool
+
+func InitGrpcClientPool(endpoint string) {
+	gonce.Do(func() {
+		pool = new(grpcPool)
+		pool.addr = endpoint
+		pool.conn = make(chan *grpc.ClientConn, 1024)
+		conn := pool.dialNew()
+		pool.conn <- conn
+	})
+}
+
+func (this *grpcPool) dialNew() *grpc.ClientConn {
+	var err error
+	var conn *grpc.ClientConn
+	conn, err = grpc.Dial(this.addr, grpc.WithInsecure())
+	if nil != err {
+		return nil
+	}
+	return conn
+}
+
+func Get() *grpc.ClientConn {
+	return pool.Get()
+}
+func (this *grpcPool) Get() *grpc.ClientConn {
+	select {
+	case conn := <-this.conn:
+		return conn
+	default:
+		return this.dialNew()
+	}
+}
+
+func Put(conn *grpc.ClientConn) error {
+	return pool.Put(conn)
+}
+
+func (this *grpcPool) Put(conn *grpc.ClientConn) error {
+	select {
+	case this.conn <- conn:
+		return nil
+	default:
+		return conn.Close()
+	}
+}
--- grpclib/server.go
+++ grpclib/server.go
@@ -1,1 +1,47 @@
 package grpclib
+
+import (
+	"fmt"
+	"net"
+	"sync"
+
+	"github.com/luoyancn/dubhe/logging"
+
+	"google.golang.org/grpc"
+)
+
+var once sync.Once
+var _grpc *grpc.Server
+
+func StartServer(port int, ss interface{}, desc ...grpc.ServiceDesc) {
+	once.Do(func() {
+		logging.LOG.Infof("Start grpc server and listen on %d\n", port)
+		listener, err := net.Listen(
+			"tcp", fmt.Sprintf("%s:%d", "0.0.0.0", port))
+		if nil != err {
+			logging.LOG.Panicf(
+				"Cannot start grpc server on port %d:%v\n", port, err)
+		}
+
+		// In general, we registe service into grpc like follows:
+		// messages.RegisterReQRePServer(_grpc,
+		// &messages.Service{HostName: host_name, ListenPort: port})
+
+		// While, for split grpc server and service, we could use follows
+		// in codes. Be care for these above, if we use like follows, we
+		// must modify the pb.go files manually, only change _xxx_serviceDesc
+		// to xxx_serviceDesc. Remind, the second params is the grpc service
+		// entity.
+		_grpc = grpc.NewServer()
+		for _, d := range desc {
+			_grpc.RegisterService(&d, ss)
+		}
+		_grpc.Serve(listener)
+	})
+}
+
+func StopServer() {
+	if nil != _grpc {
+		_grpc.Stop()
+	}
+}
